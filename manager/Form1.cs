603acad39e8b2f4bf4d02a7757d9d281d2549a98
beatriz <beatr@BIA>
2023-08-20 19:08:32 -0300

    Correção dos filtros e adição de comentários

--- manager/Form1.cs
+++ manager/Form1.cs
@@ -7,13 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace manager
 {
     public partial class Form1 : Form
     {
+        //Lista com todas as tarefas adicionadas
         private List<string> texto = new List<string>();
+        //Lista de tarefas checked
         private List<string> concluidos = new List<string>();
+
+        //Indica o Filtro anterior Selecionado
+        // 0 = todos, 1 = Concluido, 3 = pendentes
+        private int filtroAnterior = 0;
 
         public Form1()
         {
@@ -22,25 +29,28 @@ namespace manager
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            //Adiciona o novo item na checklistBox
             Tasks.Items.Add(txtNewTask.Text);
+            //Adiciona o item na lista de todas as tarefas
             texto.Add(txtNewTask.Text);
+
             txtNewTask.Text = "";
             txtNewTask.Focus();
 
         }
 
+        //Filtro de tarefas Concluidas
         private void radioButton2_Click(object sender, EventArgs e)
         {
-            foreach (var task in Tasks.CheckedItems)
-            {
-                if (!concluidos.Contains(task))
-                {
-                    concluidos.Add(task.ToString());
-                }
-            }
+            //Atualiza quais são as tarefas concluidas na Lista
+            AtualizarListaTasksConcluidas();
 
+            filtroAnterior = 1;
+
+            //Limpa a Exibição atual do CheckListBox
             Tasks.Items.Clear();
 
+            //Exibe apenas concluidas
             foreach (var t in texto)
             {
                 if (concluidos.Contains(t))
@@ -50,19 +60,15 @@ namespace manager
             }
         }
 
+        //Filtro de tarefas Pendentes
         private void radioButton3_Click(object sender, EventArgs e)
         {
-
-            foreach (var task in Tasks.CheckedItems)
-            {
-                if (!concluidos.Contains(task))
-                {
-                    concluidos.Add(task.ToString());
-                }
-            }
-
+            AtualizarListaTasksConcluidas();
             Tasks.Items.Clear();
 
+            filtroAnterior = 2;
+
+            //Exibe apenas as que não estão na lista de concluidas
             foreach (var t in texto)
             {
                 if (!concluidos.Contains(t))
@@ -72,9 +78,13 @@ namespace manager
             }
         }
 
+        //Todos da Lista
         private void radioButton1_Click(object sender, EventArgs e)
         {
+            AtualizarListaTasksConcluidas();
             Tasks.Items.Clear();
+
+            filtroAnterior = 0;
 
             foreach (var t in texto)
             {
@@ -90,6 +100,24 @@ namespace manager
 
         }
 
+        //Atualiza quais são as tarefas concluidas na Lista
+        private void AtualizarListaTasksConcluidas()
+        {
+            //caso o filtro anterior seja o pendente a lista não pode ser limpa, se não perdemos os concluidos
+            if (filtroAnterior == 0 || filtroAnterior == 1)
+            {   //Limpo a lista e readiciono
+                concluidos.Clear();
+            }
+
+            foreach (var task in Tasks.CheckedItems)
+            {
+                if (!concluidos.Contains(task))
+                {
+                    concluidos.Add(task.ToString());
+                }
+            }
+
+        }
 
     }
 }
